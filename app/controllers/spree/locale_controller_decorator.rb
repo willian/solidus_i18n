23913577540dd3378c59487b171257fe0810e8bc
Willian Fernandes <willian@willianfernandes.com.br>
2018-01-30 14:13:01 -0200

    Fix installation when not using `solidus_frontend`
    When not using `solidus_frontend` the following error happens when
    running `bin/rails g solidus_i18n:install`:
    
    uninitialized constant Spree::LocaleController (NameError)

--- app/controllers/spree/locale_controller_decorator.rb
+++ app/controllers/spree/locale_controller_decorator.rb
@@ -1,6 +1,5 @@
-Spree::LocaleController.class_eval do
-    def set
-      redirect_to root_path(locale: params[:switch_to_locale])
-    end
+class Spree::LocaleController
+  def set
+    redirect_to root_path(locale: params[:switch_to_locale])
+  end
 end
-
